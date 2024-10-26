c4391bb66ce132e629abecb5c09651c89e81eb48
alakode-nikhil <alakodenikhil@gmail.com>
2024-10-26 17:45:50 +0530

    add urlpatterns

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,9 @@
-
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('create-recipe/', CreateRecipe.as_view(), name='create-recipe'),
+    path('list-recipe/<int:pk>/', ListRecipe.as_view(), name='list-recipe'),
+    path('update-recipe/<int:pk>/', UpdateRecipe.as_view(), name='update-recipe'),
+    path('delete-recipe/<int:pk>', DeleteRecipe.as_view(), name='delete-recipe'),
 ]
