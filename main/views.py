f08ab4b5a033c0a1515dba47b18836f5961c5042
alakode-nikhil <alakodenikhil@gmail.com>
2024-10-26 17:27:41 +0530

    add CRUD views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .serializers import *
+from .models import *
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class CreateRecipe(generics.ListCreateAPIView):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [AllowAny]
+
+class ListRecipe(generics.RetrieveAPIView):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipeSerializer
+
+class UpdateRecipe(generics.RetrieveUpdateAPIView):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipeSerializer
+
+class DeleteRecipe(generics.DestroyAPIView):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipeSerializer
